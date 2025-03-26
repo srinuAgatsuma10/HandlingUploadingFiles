14b40236400cbda263dbeff59d6f266e00f769ab
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-26 21:40:07 +0530

    automationBlogSpot setup

---
+++ src/test/java/automationBlogSpot/HandlingUploadFiles.java
@@ -0,0 +1,39 @@
+package automationBlogSpot;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class HandlingUploadFiles {
+	WebDriver driver;
+
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+	}
+
+	@Test(priority = 1)
+	public void uploadSingleFile() {
+
+	}
+
+	@Test(priority = 2)
+	public void uploadMultipleFiles() {
+
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+}
