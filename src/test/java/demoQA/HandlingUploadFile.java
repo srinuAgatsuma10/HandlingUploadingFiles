916194cbf2e2cc3f5b7b109e57bb204120506972
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-26 21:35:54 +0530

    Approach Two

--- src/test/java/demoQA/HandlingUploadFile.java
+++ src/test/java/demoQA/HandlingUploadFile.java
@@ -11,26 +11,33 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class HandlingUploadFile {
-	
+
 	WebDriver driver;
-	
+
 	@BeforeClass
 	public void setUp() {
 		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://demoqa.com/upload-download");
-//		driver.manage().window().maximize();
+		driver.manage().window().maximize();
 	}
-	
-	@Test
-	public void uploadSingleFile() {
+
+	@Test(priority = 1)
+	public void uploadFileApproachOne() {
 		// Approach One
 		WebElement chooseFile = driver.findElement(By.xpath("//input[@id='uploadFile']"));
-		String path = "E:\\Sailing_To_GitHub\\HandlingUploadingFiles\\FIlesForUploading\\TestNG_Theory.txt";
+		String path = "----Enter File Path----";
 		chooseFile.sendKeys(path);
 	}
-	
+
+	@Test(priority = 2)
+	public void uploadFileApproachTwo() {
+		// Approach Two
+		WebElement chooseFile = driver.findElement(By.xpath("//input[@id='uploadFile']"));
+		chooseFile.sendKeys("----Enter File Path----");
+	}
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
